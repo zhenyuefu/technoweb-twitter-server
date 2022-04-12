40f3afd966437b3b8d8e81066e51448bee1115e5
zhenyue <zhenyuef@gmail.com>
2022-04-12 15:31:00 +0200

    auth删除不必要的代码

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -49,7 +49,7 @@ router.get("/", (req, res) => {
   if (req.isAuthenticated()) {
     return res.send({
       auth: true,
-      id: req.user.id,
+      uid: req.user.id,
       username: req.user.username,
     });
   }
@@ -82,20 +82,6 @@ router.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
-// (req, res, next) => {
-//   req.session.save((err) => {
-//     if (err) {
-//       return next(err);
-//     }
-
-//     res.send({
-//       message: "Login successful",
-//       user: req.user,
-//     });
-//   });
-// }
-// );
-
 router.post("/logout", async (req, res, next) => {
   req.logout();
   req.session.save((err) => {
@@ -105,38 +91,5 @@ router.post("/logout", async (req, res, next) => {
     res.send({ message: "Logout successful" });
   });
 });
-
-// async (req, res) => {
-//   const { username, password } = req.body;
-//   if (!username || !password) {
-//     return res.status(422).json({
-//       message: "username, password are required",
-//     });
-//   }
-//   try {
-//     const user = await User.findOne({ username });
-//     if (!user) {
-//       return res.status(422).json({
-//         message: "username or password is incorrect",
-//       });
-//     }
-//     if (!bcrypt.compareSync(password, user.password)) {
-//       return res.status(422).json({
-//         message: "username or password is incorrect",
-//       });
-//     }
-//     req.session.userId = String(user._id);
-//     res.send({
-//       user,
-//       message: "login success",
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({
-//       message: "Internal server error",
-//     });
-//   }
-// }
-// );
 
 export = router;
